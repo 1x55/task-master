48d0b25f8626d1925658954a1f40104dbda0e9db
1x55 <k4i@pm.me>
2023-08-31 17:09:21 +0900

    connected to mongoose db

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
   const express = require('express');
+  const colors = require('colors')
   require('dotenv').config();
   
   const { graphqlHTTP } = require('express-graphql');
   const schema = require('./schema/schema');
+  const connectDB = require('./config/db');
   const port = process.env.PORT || 5002;
 
   const app = express();
+  //connect to DB
+  connectDB();
 
   // GraphiQL is useful during testing and development but should be disabled in production by default. If you are using express-graphql, you can toggle it based on the NODE_ENV environment variable:
   app.use('/graphql', graphqlHTTP({
