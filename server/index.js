a3e85739fc5397c29475af9ee168976b72396565
1x55 <k4i@pm.me>
2023-08-31 11:40:31 +0900

    added sample test data before connecting to mongoDB

--- server/index.js
+++ server/index.js
@@ -2,13 +2,15 @@
   require('dotenv').config();
 
 
-  const {graphqlHTTP} = require('express-graphql') 
+  const {graphqlHTTP} = require('express-graphql');
+  const schema = require('.schema/schema');
   const port = process.env.PORT || 5000;
   const app = express();
 
 
   app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENC == 'deelopement'
 
-    
   })) 
   app.listen(port, console.log(`server running on port ${port}`));
