00e0086026e396e1c9cf368ba05ea80709aedf17
1x55 <k4i@pm.me>
2023-08-31 16:26:35 +0900

    add Project type to schema

--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
   const express = require('express');
   require('dotenv').config();
+  
+  const { graphqlHTTP } = require('express-graphql');
+  const schema = require('./schema/schema');
+  const port = process.env.PORT || 5002;
 
-
-  const {graphqlHTTP} = require('express-graphql');
-  const schema = require('.schema/schema');
-  const port = process.env.PORT || 5000;
   const app = express();
 
-
+  // GraphiQL is useful during testing and development but should be disabled in production by default. If you are using express-graphql, you can toggle it based on the NODE_ENV environment variable:
   app.use('/graphql', graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENC == 'deelopement'
+    graphiql: process.env.NODE_ENV === 'development',
+  }));
 
-  })) 
+  console.log(`Using port: ${port}`); 
   app.listen(port, console.log(`server running on port ${port}`));
+  
