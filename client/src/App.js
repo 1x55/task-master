d4d970493c96585154fead7eea4feb7969517b6f
1x55 <k4i@pm.me>
2023-09-01 12:06:17 +0900

    updated port 5005

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
+import Header from "./components/header";
+import { AppoloProvider, AppoloClient, InMemoryCache }
+from '@apollo/client';
 
-
+const client = new AppoloClient({
+  uri: 'http://localhost:5005/graphql',
+})
 function App() {
   return (
+    <>
+    <Header /> 
     <div className="container">
       <h1>Hello World</h1>
     </div>
+    </>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,22 +1,25 @@
-  const express = require('express');
-  const colors = require('colors')
-  require('dotenv').config();
-  
-  const { graphqlHTTP } = require('express-graphql');
-  const schema = require('./schema/schema');
-  const connectDB = require('./config/db');
-  const port = process.env.PORT || 5002;
+const express = require('express');
+const colors = require('colors');
+const cors = require('cors');
+require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const connectDB = require('./config/db');
+const port = process.env.PORT || 5005;
 
-  const app = express();
-  //connect to DB
-  connectDB();
+const app = express();
 
-  // GraphiQL is useful during testing and development but should be disabled in production by default. If you are using express-graphql, you can toggle it based on the NODE_ENV environment variable:
-  app.use('/graphql', graphqlHTTP({
+// Connect to database
+connectDB();
+
+app.use(cors());
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === 'development',
-  }));
+  })
+);
 
-  console.log(`Using port: ${port}`); 
-  app.listen(port, console.log(`server running on port ${port}`));
-  
+app.listen(port, console.log(`Server running on port ${port}`));
