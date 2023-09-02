16a973651e5b6a79ea8353fe219661babe980a95
1x55 <k4i@pm.me>
2023-09-02 17:47:13 +0900

    set up single project query functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from "./components/Header";
-import { ApolloProvider, ApolloClient, InMemoryCache, from } from '@apollo/client';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
+import Project from './pages/Project';
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -36,6 +37,7 @@ function App() {
     <div className="container">
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/projects/:id' element={<Project />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
