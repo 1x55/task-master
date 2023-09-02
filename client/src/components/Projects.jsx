a09312cc1a19ed9b872ae8fdc240b5edd89783be
1x55 <k4i@pm.me>
2023-09-02 16:25:58 +0900

    updated project cards

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,9 +1,10 @@
 import Spinner from "./Spinner"
 import { useQuery } from "@apollo/client"
-import { GET_Projects } from "../queries/ProjectQueries"
+import ProjectCard from "./ProjectCard";
+import { GET_PROJECTS } from "../queries/projectQueries"
 
 export default function Projects() {
-  const { loading, error, data } = useQuery(GET_Projects);
+  const { loading, error, data } = useQuery(GET_PROJECTS);
   
   if (loading) return <Spinner />;
   if (error) return <p>Something went wrong</p>
@@ -11,7 +12,7 @@ export default function Projects() {
   
   return <>
     {data.projects.length > 0 ? (
-           <div className="row">
+           <div className="row mt-4">
                 { data.projects.map((project) => (
                     <ProjectCard key={project.id} project={project} />
                 )) }
