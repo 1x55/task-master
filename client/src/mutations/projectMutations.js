fa30d285e5029e32f5ddc21c8a9122db9b096c9a
1x55 <k4i@pm.me>
2023-09-04 15:42:02 +0900

    completed project deleteButton

--- client/src/mutations/projectMutations.js
+++ client/src/mutations/projectMutations.js
@@ -1,29 +1,38 @@
-  import {gql} from '@apollo/client';
+import { gql } from '@apollo/client';
 
-  const ADD_PROJECT = gql`
-    mutation AddProject(
-        $name: String!, 
-        $description: String!, 
-        $status: ProjectStatus!, 
-        $clientId: ID!
-        ){
-        addProject(
-            name: $name, 
-            description: $description, 
-            status: $status,
-            clientId: $clientId 
-        ) {
-            id
-            name
-            description
-            status
-            client {
-                id 
-                name
-                email
-                phone
-            }
-        }
+const ADD_PROJECT = gql`
+  mutation AddProject(
+    $name: String!
+    $description: String!
+    $status: ProjectStatus!
+    $clientId: ID!
+  ) {
+    addProject(
+      name: $name
+      description: $description
+      status: $status
+      clientId: $clientId
+    ) {
+      id
+      name
+      description
+      status
+      client {
+        id
+        name
+        email
+        phone
+      }
     }
-`
-  export { ADD_PROJECT };
+  }
+`;
+
+const DELETE_PROJECT = gql`
+  mutation DeleteProject($id: ID!) {
+    deleteProject(id: $id) {
+      id
+    }
+  }
+`;
+
+  export { ADD_PROJECT, DELETE_PROJECT };
